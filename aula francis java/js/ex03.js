1afc77933999cddbceccdef24824a96fd3fda6c5
Carlos Eduardo da Silva <107575864+bruno-emanuel-almeida@users.noreply.github.com>
2022-11-16 16:17:27 -0300

    1231231

---
+++ aula francis java/js/ex03.js
@@ -0,0 +1,10 @@
+var carlos, carl, cj, karlin
+
+carlos=prompt("Qualé teu nome?")
+carl=parseFloat (prompt(carlos+" Digite ai o primeiro valor meu amigo"))
+cj=parseFloat (prompt(carlos+" Digite mais um número ai pro teu cria"))
+karlin=carl+cj
+
+alert(karlin)
+
+document.write("<h2> a namorada do carlos tem "+karlin+" anos</h2>")
---
+++ aula francis java/js/ex07.js
@@ -0,0 +1,19 @@
+var a, b, c, d, e
+
+a=prompt("Qual seu nome minha bela dama?")
+b=prompt("Qual sua idade minha bela moça?")
+
+
+
+if(b>=18)
+{
+    c=prompt("Qual seu cpf minha linda?")
+    document.write("<h2>Seu cpf é"+c+"</h2>")
+    
+}
+
+else{
+
+    document.write("<h2>Você é menor de idade, vá pro frifas</h2>")
+}
+
